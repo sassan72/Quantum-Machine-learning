b7dc2db97a16c9100c49f537318b6709add98758
sassan72 <sassan.moradi@gmail.com>
2021-09-23 10:38:03 +0200

    Add files via upload

---
+++ g_score.py
@@ -0,0 +1,81 @@
+import numpy as np
+from numpy.linalg import matrix_rank
+import pennylane as qml
+import sklearn.decomposition
+from sklearn import metrics
+import pandas as pd
+from sklearn.metrics.pairwise import linear_kernel
+from scipy.linalg import sqrtm
+from scipy.linalg.interpolative import estimate_spectral_norm
+
+# this code should be run on classical computer
+
+data1 = pd.read_csv(r'Path where the CSV file is stored\File TDS.csv') 
+
+data2 = pd.read_csv(r'Path where the CSV file is stored\File TLD.csv') 
+
+data3 = pd.read_csv(r'Path where the CSV file is stored\File VDS.csv') 
+
+data4 = pd.read_csv(r'Path where the CSV file is stored\File VLD.csv') 
+
+y_train= data2[:,] 
+
+X_train2=data1[:]
+
+y_test= data4[:,] 
+
+X_test2=data3[:]
+
+
+pi = np.pi
+
+
+X_train1 = sklearn.preprocessing.normalize(X_train2, norm='l2',axis=0)
+
+X_test1 = sklearn.preprocessing.normalize(X_test2, norm='l2', axis=0)
+
+X_train = sklearn.preprocessing.normalize(X_train1, norm='l2',axis=1)
+
+X_test = sklearn.preprocessing.normalize(X_test1, norm='l2', axis=1)
+
+feature_size = 8 # len(X_train[0])
+
+
+n_qubits = 3 # log(feature_size) 
+
+dev_kernel = qml.device("default.qubit", wires=n_qubits)
+
+
+projector = np.zeros((2**n_qubits, 2**n_qubits))
+projector[0, 0] = 1
+
+@qml.qnode(dev_kernel)
+def kernel(x1, x2):
+    """The quantum kernel."""
+    qml.templates.MottonenStatePreparation(x1, wires=range(n_qubits))
+    qml.inv(qml.templates.MottonenStatePreparation(x2, wires=range(n_qubits)))
+    return qml.expval(qml.Hermitian(projector, wires=range(n_qubits)))
+
+
+def kernel_matrix(A, B):
+    """Compute the matrix whose entries are the kernel
+       evaluated on pairwise data from sets A and B."""
+    return np.array([[kernel(a, b) for b in B] for a in A])
+
+
+K2 = kernel_matrix(X_train, X_train)
+
+
+K1 = linear_kernel(X_train, X_train)
+
+
+K22 = sqrtm(K2)
+
+K11 = np.linalg.pinv(K1)
+
+KK = (np.real(K22))*K11*(np.real(K22)) # g(K^C||K^Q)
+
+
+NORM = estimate_spectral_norm(KK, its=20)
+
+print(np.sqrt(NORM))
---
+++ qDC_Swap_Test.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pennylane as qml
+import sklearn.decomposition
+from sklearn import metrics
+import math
+import pandas as pd
+
+from qiskit import IBMQ # for run on IBMQ devices 
+
+pi = np.pi
+
+
+data1 = pd.read_csv(r'Path where the CSV file is stored\File TDS.csv') 
+
+data2 = pd.read_csv(r'Path where the CSV file is stored\File TLD.csv') 
+
+data3 = pd.read_csv(r'Path where the CSV file is stored\File VDS.csv') 
+
+data4 = pd.read_csv(r'Path where the CSV file is stored\File VLD.csv') 
+
+X_train=data1[:]
+
+y_train= data2[:,] 
+
+y_test= data4[:,] 
+
+X_test=data3[:]
+
+X_train1 = sklearn.preprocessing.normalize(X_train, norm='l2',axis=0)
+
+X_test1 = sklearn.preprocessing.normalize(X_test, norm='l2', axis=0)
+
+X_train2 = sklearn.preprocessing.normalize(X_train1, norm='l2',axis=1)
+
+X_test2 = sklearn.preprocessing.normalize(X_test1, norm='l2', axis=1)
+
+
+x1_train=[]
+x2_train=[]
+
+for i in range(len(X_train2)):
+    if y_train[i]==1:
+        a=X_train2[i]
+        x1_train.append(a)
+    else:
+        b=X_train2[i]
+        x2_train.append(b)
+
+feature_size = 8 # len(X_train[0])
+
+num_qubits = 7 # 2*log(feature_size) + 1(ancilla)
+
+provider = IBMQ.enable_account('API_KEY')
+
+dev = qml.device('qiskit.ibmq', wires=num_qubits, backend='IBMQ_DEVICE', provider=provider, shots=8192) # for IBMQ devices
+
+#dev = qml.device("default.qubit", wires=num_qubits, shots=8192) # simulator
+
+
+
+@qml.qnode(dev)
+def circuit(x1, x2):
+
+	qml.Hadamard(wires=0)
+	
+	
+	qml.templates.MottonenStatePreparation(x1, wires=[1, 2, 3])
+	qml.templates.MottonenStatePreparation(x2, wires=[4, 5, 6])
+
+	qml.CSWAP(wires=[0, 1, 4])
+	qml.CSWAP(wires=[0, 2, 5])
+	qml.CSWAP(wires=[0, 3, 6])
+	
+	qml.Hadamard(wires=0)
+
+	return qml.expval(qml.PauliZ(0))
+
+D1 = []
+
+for x1 in x1_train:
+    for x2 in X_test2:
+        a = circuit(x1, x2)
+        D1.append(a)
+
+
+D2 = []
+
+for x1 in x2_train:
+    for x2 in X_test2:
+        b = circuit(x1, x2)
+        D2.append(b)
+
+
+D11 = np.reshape(D1, (len(x1_train), len(X_test)))
+D22 = np.reshape(D2, (len(x2_train), len(X_test)))
+
+d11 = np.max(D11, axis=0)
+d22 = np.max(D22, axis=0)
+m = len(X_test)
+
+Prediction=[]
+for i in range(m):
+    if d11[i] >= d22[i]:
+        Prediction.append(1)
+    else:
+        Prediction.append(0)
+    
+BB = metrics.confusion_matrix(y_test, Prediction, labels=[0,1])
+
+print(Prediction)
+
+
+
+print(BB)   
